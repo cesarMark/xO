e65d800e1eaf78c02faf7c3d9a41ddba8c2c3d8f
Yuvali <ymbproduction@gmail.com>
2019-04-14 14:28:15 +0300

    Better version

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os
 import random
+def displayMat(mat): #Function that displays the matrix
+    for(i) in range(0,3):
+         for(j) in range(0,3):
+             print("|",matrix[i][j],end=" |")
+         print("")
+         print("________________")
+
 #This function counts if one of the players won the game by checking if the whole row/column/diagonal is full
 def checkWin(mat):
     for(i) in range(0 , 3):
         countXRow = 0
-        countXDiag = 0
+        countXDiagLeft = 0
         countORow = 0
-        countODiag = 0
+        countODiagLeft = 0
+        countXDiagRight = 0
+        countODiagRight = 0
         countOCol = 0
         countXCol = 0
         for(j) in range(0 , 3):
             if matrix[j][j] == 'X':
-                countXDiag += 1
+                countXDiagLeft += 1
+            if matrix[j][2-j] == 'X':
+                countXDiagRight += 1
+            if matrix[j][2-j] == 'O':
+                countODiagRight += 1
             if matrix[j][j] == 'O':
-                countODiag += 1
+                countODiagLeft += 1
             if matrix[i][j] == 'X':
                 countXRow += 1
             if matrix[i][j] == 'O':
@@ -24,13 +37,17 @@ def checkWin(mat):
                 countOCol += 1
         if countXRow == 3:
             return True
-        if countXDiag == 3:
+        if countXDiagRight == 3:
+            return True
+        if countODiagRight == 3:
+            return False
+        if countXDiagLeft == 3:
             return True
         if countXCol == 3:
             return True
         if countORow == 3:
             return False
-        if countODiag == 3:
+        if countODiagLeft == 3:
             return False
         if countOCol == 3:
             return False
@@ -43,17 +60,13 @@ def computerMove(mat):
         if  matrix[compRow][compColumn] != 'O' and matrix[compRow][compColumn] != 'X':
              matrix[compRow][compColumn] = 'O'
              flag = 0
+flag = 1
 matrix = ([['_','_','_'],
            ['_','_','_'],
            ['_','_','_']])
-flag = 1
 while(flag > 0): #Flag is defined as the sequence of the game 
     os.system('cls')
-    for(i) in range(0,3): #The nested loop will show up the matrix
-         for(j) in range(0,3):
-             print("|",matrix[i][j],end=" |")
-         print("")
-         print("________________")
+    displayMat(matrix)
     print("Input the coordinates [row] (space) [column]")
     x, y = map(int, input().split())
     if x > 2 or x < 0 or y > 2 or y < 0: #One of the tests that makes sure that the player didn't input any wrong coordinates
@@ -68,6 +81,8 @@ while(flag > 0): #Flag is defined as the sequence of the game
     if checkWin(matrix) == True: #Checks if the player won
         print("CONGRATZ! YOU WON!")
         flag = 0
+        displayMat(matrix)
     elif checkWin(matrix) == False: #The false reperesents the computer and if the function will return false than the computer won
         print("YOU LOST :( , TRY AGAIN")
+        displayMat(matrix)
         flag = 0
